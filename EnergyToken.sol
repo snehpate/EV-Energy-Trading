9c34b62fcd331fc6018cbd1d28dbc10f462c2772
Sneh <66151232+snehpate@users.noreply.github.com>
2025-01-06 13:27:10 -0800

    Update EnergyToken.sol

--- EnergyToken.sol
+++ EnergyToken.sol
@@ -1,11 +1,1 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-
-// EnergyToken is an ERC-20 token representing energy credits in kWh.
-contract EnergyToken is ERC20 {
-    constructor(uint256 initialSupply) ERC20("EnergyToken", "ETK") {
-        _mint(msg.sender, initialSupply * (10 ** decimals()));
-    }
-}
